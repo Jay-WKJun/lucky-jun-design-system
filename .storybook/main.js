38f0570d4642c2fe94a3220fbb297fc18e72b65b
Woo KyungJun <natcho9010@gmail.com>
2022-05-01 23:31:13 +0900

    Chore : add storybook scss plugin

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,6 +8,7 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-postcss",
+    "storybook-addon-sass-postcss",
     {
       name: '@storybook/addon-postcss',
       options: {
